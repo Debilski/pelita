ba794396d5e6aee1498ef521c33480344c9160b5
Rike-Benjamin Schuppner <rikebs@debilski.de>
2013-09-20 09:31:34 +0200

    bump

---
+++ tournament/match_tree.hs
@@ -0,0 +1,28 @@
+
+data MatchTree a = Match (MatchTree a) (MatchTree a) | Winner a
+
+instance Functor MatchTree where
+  fmap f (Winner a) = Winner (f a)
+  fmap f (Match l r) = Match (fmap f l) (fmap f r)
+
+
+data Team = Team { name :: [Char] }
+
+playGame :: (Team, Team) -> Team
+playGame (Team t1, Team t2) = if (length t1) > (length t2) then (Team t1) else (Team t2)
+
+playNextMatch :: MatchTree Team -> MatchTree Team
+playNextMatch (Winner a) = Winner a
+playNextMatch (Match l@(Winner a) r@(Winner b)) = Winner (playGame (a, b))
+playNextMatch (Match l@(Winner _) r@(Match _ _)) = Match l (playNextMatch r)
+playNextMatch (Match l@(Match _ _) r) = Match (playNextMatch l) r
+
+-- runMatches :: MatchTree Team -> Maybe Team
+-- runMatches (Match left right) = getWinner (runMatches left) (runMatches right)
+--  where
+--    getWinner Nothing Nothing = Nothing
+--    getWinner l Nothing = l
+--    getWinner Nothing r = r
+--    getWinner l r = l
+-- runMatches maybeWinner = maybeWinner
+
--- tournament/tournament2.py
+++ tournament/tournament2.py
@@ -2,10 +2,20 @@
 # -*- coding:utf-8 -*-
 from __future__ import print_function
 import random
-from subprocess import Popen
+from subprocess import Popen, PIPE
 import sys
 import time
 import cStringIO
+import ConfigParser
+
+import logging
+
+# silence stupid warnings from logging module
+logging.root.manager.emittedNoHandlerWarning = 1
+
+_logger = logging.getLogger("pelita.tournament")
+
+CFG_FILE = "./tournament.cfg"
 
 # Drop this as soon as we drop support for python 2.6
 try:
@@ -38,40 +48,109 @@ class SpeakerModule(object):
             string = stream.getvalue()
             _print(string, end='')
             sys.stdout.flush()
-            self.speak(string)
+            self._speak(string)
             time.sleep(wait)
 
-    def speak(self, test):
+    def _speak(self, test):
         pass
 
 class FliteSpeaker(SpeakerModule):
     def __init__(self, flite):
         self.flite = flite
 
-    def speak(self, text):
+    def _speak(self, text):
         Popen([self.flite, "-t", text])
 
 class OSXSpeaker(SpeakerModule):
-    def speak(self, text):
+    def _speak(self, text):
         Popen(["/usr/bin/say", text])
 
-print = OSXSpeaker().print
-
-print("abc def ghi")
-sys.exit(0)
+class NoSpeaker(SpeakerModule):
+    def _speak(self, text):
+        pass
 
 # Global random seed. Keep it fixed or it may be impossible
 # to replicate a tournament.
 # Individual matches will get a random seed derived from this.
 random.seed(42)
 
-def get_team_name(pelitagame, factory):
-    cmd = [pelitagame, "--check-team", factory]
-    pass
+class PelitaRunner(object):
+    def __init__(self, pelitagame, dry_run=False):
+        self.pelitagame = pelitagame
+        self.dry_run = dry_run
+
+    def get_team_name(self, team_spec):
+        cmd = [self.pelitagame, "--check-team", team_spec]
+        _logger.debug("cmd %r", cmd)
+        stdout, stderr = Popen(cmd, stdout=PIPE, stderr=PIPE).communicate()
+        try:
+            return stdout.splitlines()[-1]
+        except IndexError:
+            _logger.warn(stderr)
+
+    def run_match(self, spec1, spec2):
+        cmd = [self.pelitagame, spec1, spec2]
+
+
+class RoundRobin(object):
+    def __init__(self, teams):
+        self.teams = teams
+        self.results = []
+        self.to_play = [(t1, t2) if random.randint(0, 1) else (t2, t1)
+                                 for idx1, t1 in enumerate(self.teams)
+                                 for idx2, t2 in enumerate(self.teams)
+                                 if idx1 < idx2]
+
+        random.shuffle(self.to_play)
+
+    def play(self):
+        while self.to_play:
+            match = self.to_play.pop()
+            team1, team2 = match
+            result = run_match(team1, team2)
+            self.add_result(match, result)
+
+    def add_result(self, match):
+        self.results += [(match, result)]
+
+    def ranking(self):
+        print(self.results)
+
+class MatchTree(object):
+    def __init__(self, tree1, tree2):
+        self.tree1 = tree1
+        self.tree2 = tree2
+
+
+
+class KnockOutRound(object):
+    def __init__(self, ranked_teams, number_teams=4, last_chance_match=True):
+        if len(ranked_teams) <= number_teams + bool(last_chance_match):
+            raise ValueError("Not enough teams")
+
+        self.all_teams = ranked_teams
+        self.number_teams = number_teams
+        self.last_chance_match = last_chance_match
+
+        if self.last_chance_match:
+            self.last_chance_team = self.all_teams[-1]
+        self.ko_teams = self.all_teams[:self.number_teams]
+
+        if self.last_chance_match:
+            do_match(old_winner, self.last_chance_team)
+
+def start_logging(filename):
+    hdlr = logging.FileHandler(filename, mode='w')
+    logger = logging.getLogger('pelita')
+    FORMAT = \
+    '[%(relativeCreated)06d %(name)s:%(levelname).1s][%(funcName)s] %(message)s'
+    formatter = logging.Formatter(FORMAT)
+    hdlr.setFormatter(formatter)
+    logger.addHandler(hdlr)
+    logger.setLevel(logging.DEBUG)
 
 if __name__ == '__main__':
     # Command line argument parsing.
-    # Oh, why must argparse be soo verbose :(
     parser = argparse.ArgumentParser(description='Run a tournament',
                                  add_help=False,
                                  formatter_class=argparse.RawDescriptionHelpFormatter)
@@ -80,24 +159,47 @@ if __name__ == '__main__':
     parser._optionals = parser.add_argument_group('Options')
     parser.add_argument('--help', '-h', help='show this help message and exit',
                         action='store_const', const=True)
+    parser.add_argument('--verbose', '-v', help='print before executing commands',
+                        action='store_const', const=True)
+    parser.add_argument('--dry-run', help='do not actually run the matches',
+                        action='store_const', const=True)
+    parser.add_argument('--cfg', help='path to the configuration file',
+                        default=CFG_FILE)
     parser.add_argument('--speak', '-s', help='speak loudly every messsage on stdout',
                         action='store_const', const=True)
     parser.add_argument('--rounds', '-r', help='maximum number of rounds to play per match',
                         type=int, default=300)
-    parser.add_argument('--viewer', '-v', help='the pelita viewer to use',
+    parser.add_argument('--viewer', help='the pelita viewer to use',
                         default='tk')
     parser.add_argument('--teams', help='load teams from TEAMFILE',
                     metavar="TEAMFILE.json", default="teams.json")
-    parser.epilog = """
-TEAMFILE.json must be of the form:
-    { "group0": ["Name0", "Name1", "Name2"],
-      "group1": ["Name0", "Name1", "Name2"],
-      "group2": ["Name0", "Name1", "Name2"],
-      "group3": ["Name0", "Name1", "Name2"],
-      "group4": ["Name0", "Name1", "Name2"]
-    }
-"""
     args = parser.parse_args()
     if args.help:
         parser.print_help()
         sys.exit(0)
+
+    if args.verbose:
+        start_logging("/dev/stdout")
+
+    if args.speak:
+        print = OSXSpeaker().print
+    else:
+        print = NoSpeaker().print
+
+    config = ConfigParser.RawConfigParser()
+    config.read(args.cfg)
+
+    teams = config.items("teams")
+    teams = dict(teams).values()
+
+    runner = PelitaRunner(args.pelitagame, args.dry_run)
+    spec_names = {}
+    for team in teams:
+        spec_names[team] = runner.get_team_name(team)
+
+    for (t1, t2) in RoundRobin(spec_names.keys()):
+        print(t1)
+
+    for team in spec_names.values():
+        print(runner.get_team_name(team))
+
