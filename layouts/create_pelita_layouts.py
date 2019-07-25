afcf828beb0f8300f41776b695cf6c5f4fe477b8
Rike-Benjamin Schuppner <rikebs@debilski.de>
2019-07-25 15:55:17 +0200

    ENH: Use pathlib

--- layouts/create_pelita_layouts.py
+++ layouts/create_pelita_layouts.py
@@ -1,34 +1,34 @@
 #!/usr/bin/env python3
 # Use this script to update/regenerate the layouts strings in pelita.layouts.py
 
-import os
-import zlib
 import base64
+from pathlib import Path
+import zlib
+
 import pelita
 
 EXTENSION = '.layout'
 OUTFILENAME = '__layouts.py'
 
-local_dir = os.path.dirname(os.path.realpath(__file__))
-pelita_path = os.path.dirname(pelita.__file__)
-outfile = os.path.join(pelita_path, OUTFILENAME)
+local_dir = Path(__file__).resolve().parent
+pelita_path = Path(pelita.__file__).parent
+outfile = pelita_path / OUTFILENAME
 
 layout_entry = '{name} = """{code}"""\n'
 
 content = '### This file is auto-generated. DO NOT EDIT! ###\n'
 # loop through all layout files
-for f in sorted(os.listdir(local_dir)):
-    flname, ext = os.path.splitext(f)
+for f in sorted(local_dir.iterdir()):
+    flname, ext = f.stem, f.suffix
     if ext != EXTENSION:
         continue
-    with open(os.path.join(local_dir,f), 'rb') as bytemaze:
-        layout = bytemaze.read()
+    layout = f.read_bytes()
 
-        layout_name = "layout_" + flname
-        # encode layout string
-        layout_code = base64.encodebytes(zlib.compress(layout)).decode()
+    layout_name = "layout_" + flname
+    # encode layout string
+    layout_code = base64.encodebytes(zlib.compress(layout)).decode()
 
-        content += layout_entry.format(name=layout_name, code=layout_code)
+    content += layout_entry.format(name=layout_name, code=layout_code)
 
 # write out file in pelita directory
 with open(outfile, 'w') as out:
